dd2480aa8f932fd639098071d06ab2ae00757791
Felix <felix.fuellgraf@neuland-bfi.de>
2014-06-25 16:33:50 +0200

    weiter..

--- exonode/public/javascripts/painter.js
+++ exonode/public/javascripts/painter.js
@@ -4,4 +4,8 @@ canvas.height = window.innerHeight-200;
 
 $.get("/systemlist", function(data){
 	systems = data;
+	for(var i=0; i<systems.length; i++){
+		cur = systems[i];
+		console.log(cur.system.name[0]);
+	}
 });
